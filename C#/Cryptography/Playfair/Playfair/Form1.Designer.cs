bd42dd1c385ec222ae2741b2a76010901bdded9d
MuradyanEduard <115282538+MuradyanEduard@users.noreply.github.com>
2023-02-07 01:04:04 +0400

    Playfair First Commit

---
+++ C#/Cryptography/Playfair/Playfair/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Playfair
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.TxtSentence = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtKey = new System.Windows.Forms.TextBox();
+            this.labelAnswer = new System.Windows.Forms.TextBox();
+            this.ChBoxEncrypt = new System.Windows.Forms.CheckBox();
+            this.ChBoxDecrypt = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TxtSentence
+            // 
+            this.TxtSentence.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.TxtSentence.Location = new System.Drawing.Point(32, 182);
+            this.TxtSentence.Name = "TxtSentence";
+            this.TxtSentence.Size = new System.Drawing.Size(395, 30);
+            this.TxtSentence.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.btnStart.Location = new System.Drawing.Point(342, 112);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(85, 30);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.Location = new System.Drawing.Point(27, 154);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Sentence";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(459, 48);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(310, 260);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label2.Location = new System.Drawing.Point(27, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Key";
+            // 
+            // TxtKey
+            // 
+            this.TxtKey.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.TxtKey.Location = new System.Drawing.Point(32, 112);
+            this.TxtKey.Name = "TxtKey";
+            this.TxtKey.Size = new System.Drawing.Size(201, 30);
+            this.TxtKey.TabIndex = 4;
+            // 
+            // labelAnswer
+            // 
+            this.labelAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.labelAnswer.Location = new System.Drawing.Point(32, 242);
+            this.labelAnswer.Name = "labelAnswer";
+            this.labelAnswer.Size = new System.Drawing.Size(395, 30);
+            this.labelAnswer.TabIndex = 7;
+            // 
+            // ChBoxEncrypt
+            // 
+            this.ChBoxEncrypt.AutoSize = true;
+            this.ChBoxEncrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.ChBoxEncrypt.Location = new System.Drawing.Point(32, 48);
+            this.ChBoxEncrypt.Name = "ChBoxEncrypt";
+            this.ChBoxEncrypt.Size = new System.Drawing.Size(97, 29);
+            this.ChBoxEncrypt.TabIndex = 8;
+            this.ChBoxEncrypt.Text = "Encrypt";
+            this.ChBoxEncrypt.UseVisualStyleBackColor = true;
+            this.ChBoxEncrypt.CheckedChanged += new System.EventHandler(this.ChBoxEncrypt_CheckedChanged);
+            // 
+            // ChBoxDecrypt
+            // 
+            this.ChBoxDecrypt.AutoSize = true;
+            this.ChBoxDecrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.ChBoxDecrypt.Location = new System.Drawing.Point(166, 48);
+            this.ChBoxDecrypt.Name = "ChBoxDecrypt";
+            this.ChBoxDecrypt.Size = new System.Drawing.Size(98, 29);
+            this.ChBoxDecrypt.TabIndex = 9;
+            this.ChBoxDecrypt.Text = "Decrypt";
+            this.ChBoxDecrypt.UseVisualStyleBackColor = true;
+            this.ChBoxDecrypt.CheckedChanged += new System.EventHandler(this.ChBoxDecrypt_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(824, 421);
+            this.Controls.Add(this.ChBoxDecrypt);
+            this.Controls.Add(this.ChBoxEncrypt);
+            this.Controls.Add(this.labelAnswer);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtKey);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.TxtSentence);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Playfair";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtSentence;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtKey;
+        private System.Windows.Forms.TextBox labelAnswer;
+        private System.Windows.Forms.CheckBox ChBoxEncrypt;
+        private System.Windows.Forms.CheckBox ChBoxDecrypt;
+    }
+}
+
---
+++ C#/Cryptography/Playfair/Playfair/Form1.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Playfair
+{
+    public partial class Form1 : Form
+    {
+        String StrDefult = "ABCDEFGHIKLMNOPQRSTUVWXYZ";
+        String Pass = "",newPass ="";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.ColumnCount = 5;
+            this.dataGridView1.DefaultCellStyle.Font = new Font("Tahoma", 20);
+
+            for (int i = 0; i < 5; i++)
+            {
+                DataGridViewColumn column = dataGridView1.Columns[i];
+                column.Width = 45;
+                dataGridView1.RowTemplate.Height = 45;
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            StrDefult = "ABCDEFGHIKLMNOPQRSTUVWXYZ";
+
+            if (ChBoxDecrypt.Checked == false && ChBoxEncrypt.Checked == false)
+            {
+                MessageBox.Show("Select metod!");
+                return;
+            }
+
+            if (TxtKey.Text.Length > 25) 
+            {
+                MessageBox.Show("Key length must be little then 25!");
+                return;
+            }
+
+            for (int i = 0; i < TxtKey.Text.Length; i++)
+                if (Char.ToUpper(TxtKey.Text[i]) < 'A' || Char.ToUpper(TxtKey.Text[i]) > 'Z')
+                {
+                    MessageBox.Show("Key is wrong!");
+                    return;
+                }
+
+            for (int i = 0; i < TxtSentence.Text.Length; i++)
+                if ((Char.ToUpper(TxtSentence.Text[i]) < 'A' || Char.ToUpper(TxtSentence.Text[i]) > 'Z') && !TxtSentence.Text[i].Equals(' '))
+                {
+                    MessageBox.Show("Text Sentence is wrong!");
+                    return;
+                }
+
+            Pass = "";
+            newPass = "";
+            for (int i = 0; i < TxtKey.Text.Length; i++)
+            {
+                if (Char.ToUpper(TxtKey.Text[i]).Equals('J'))
+                    Pass = Pass + Char.ToUpper('I');
+                else
+                    Pass = Pass + Char.ToUpper(TxtKey.Text[i]);
+
+                for (int j = 0; j < StrDefult.Length; j++)
+                    if (Char.ToUpper(StrDefult[j]).Equals(Char.ToUpper(TxtKey.Text[i])))
+                        StrDefult = StrDefult.Substring(0,j) + StrDefult.Substring(j + 1, StrDefult.Length - 1 - j);
+            }
+
+            Pass = Pass + StrDefult;
+            
+            for (int i = 0; i < Pass.Length; i++)
+            {
+                Boolean stugox = true;
+                for (int j = 0; j < newPass.Length; j++)
+                {
+                    if (Pass[i].Equals(newPass[j]))
+                        stugox = false;
+                }
+
+                if (stugox)
+                    newPass = newPass + Pass[i];
+            }
+
+
+            String Answer = "", StrSentence = TxtSentence.Text;
+            String str1 = "", str2 = "";
+            labelAnswer.Text = "";
+
+            for (int j = 0; j < StrSentence.Length; j++)
+                if (StrSentence[j].Equals(' '))
+                    StrSentence = StrSentence.Substring(0, j) + StrSentence.Substring(j + 1, StrSentence.Length - 1 - j);
+
+            Pass = newPass;
+            dataGridView1.Rows.Clear();
+
+            for (int i = 0; i < 5; i++)
+            {
+                this.dataGridView1.Rows.Add("A", "B", "C", "D", "E");
+
+                for (int j = 0; j < 5; j++)
+                {
+                    dataGridView1.Rows[i].Cells[j].Value = Pass.Substring(0,1);
+                    Pass = Pass.Substring(1);
+                }
+            }
+
+
+
+            for (int i = 0; i < StrSentence.Length; i++)
+            {
+
+                if (StrSentence[i].Equals(' '))
+                {
+                    StrSentence = StrSentence.Substring(0, i) + StrSentence.Substring(i + 1, StrSentence.Length - 1 - i);
+                    i--;
+                }
+            }
+
+
+            if (StrSentence.Length % 2 != 0)
+                StrSentence = StrSentence + "Z";
+            
+
+            if (ChBoxEncrypt.Checked == true)
+            {                
+                while (StrSentence.Length != 0)
+                {
+
+                    str1 = StrSentence.Substring(0, 1);
+                    str2 = StrSentence.Substring(1, 1);
+
+                    if (Char.ToUpper(str1[0]).Equals('J'))
+                        str1 = "" + 'I';
+
+                    if (Char.ToUpper(str2[0]).Equals('J'))
+                        str1 = "" + 'I';
+
+
+                    if (Char.ToUpper(str1[0]).Equals(Char.ToUpper(str2[0])) && !Char.ToUpper(str1[0]).Equals('Z'))
+                    {
+                        StrSentence = StrSentence + Char.ToUpper(str1[0]) + "Z" + Char.ToUpper(str2[0]) + "Z";
+                        StrSentence = StrSentence.Substring(2);
+                        continue;
+
+                    }
+
+                    int[] pos1XY = LetterSearch(str1[0]);
+                    int[] pos2XY = LetterSearch(str2[0]);
+
+                    if (pos1XY[0] == pos2XY[0])
+                    {
+                        if (pos1XY[1] == 4)
+                            pos1XY[1] = 0;
+                        else
+                            pos1XY[1] = pos1XY[1] + 1;
+
+                        if (pos2XY[1] == 4)
+                            pos2XY[1] = 0;
+                        else
+                            pos2XY[1] = pos2XY[1] + 1;
+
+                        Answer = Answer + dataGridView1.Rows[pos1XY[0]].Cells[pos1XY[1]].Value;
+                        Answer = Answer + dataGridView1.Rows[pos2XY[0]].Cells[pos2XY[1]].Value + " ";
+                        StrSentence = StrSentence.Substring(2);
+                        continue;
+                    }
+
+                    if (pos1XY[1] == pos2XY[1])
+                    {
+                        if (pos1XY[0] == 4)
+                            pos1XY[0] = 0;
+                        else
+                            pos1XY[0] = pos1XY[0] + 1;
+
+                        if (pos2XY[0] == 4)
+                            pos2XY[0] = 0;
+                        else
+                            pos2XY[0] = pos2XY[0] + 1;
+
+                        Answer = Answer + dataGridView1.Rows[pos1XY[0]].Cells[pos1XY[1]].Value;
+                        Answer = Answer + dataGridView1.Rows[pos2XY[0]].Cells[pos2XY[1]].Value + " ";
+                        StrSentence = StrSentence.Substring(2);
+                        continue;
+                    }
+
+                    Answer = Answer + dataGridView1.Rows[pos1XY[0]].Cells[pos2XY[1]].Value;
+                    Answer = Answer + dataGridView1.Rows[pos2XY[0]].Cells[pos1XY[1]].Value + " ";
+                    StrSentence = StrSentence.Substring(2);
+                }
+            }
+            else
+                while (StrSentence.Length != 0)
+                {
+                    str1 = StrSentence.Substring(0, 1);
+                    str2 = StrSentence.Substring(1, 1);
+
+                    int[] pos1XY = LetterSearch(str1[0]);
+                    int[] pos2XY = LetterSearch(str2[0]);
+
+                    if (pos1XY[0] == pos2XY[0])
+                    {
+                        if (pos1XY[1] == 0)
+                            pos1XY[1] = 4;
+                        else
+                            pos1XY[1] = pos1XY[1] - 1;
+
+                        if (pos2XY[1] == 0)
+                            pos2XY[1] = 4;
+                        else
+                            pos2XY[1] = pos2XY[1] - 1;
+
+                        Answer = Answer + dataGridView1.Rows[pos1XY[0]].Cells[pos1XY[1]].Value;
+                        Answer = Answer + dataGridView1.Rows[pos2XY[0]].Cells[pos2XY[1]].Value + " ";
+                        StrSentence = StrSentence.Substring(2);
+                        continue;
+                    }
+
+                    if (pos1XY[1] == pos2XY[1])
+                    {
+                        if (pos1XY[0] == 0)
+                            pos1XY[0] = 4;
+                        else
+                            pos1XY[0] = pos1XY[0] - 1;
+
+                        if (pos2XY[0] == 0)
+                            pos2XY[0] = 4;
+                        else
+                            pos2XY[0] = pos2XY[0] - 1;
+
+                        Answer = Answer + dataGridView1.Rows[pos1XY[0]].Cells[pos1XY[1]].Value;
+                        Answer = Answer + dataGridView1.Rows[pos2XY[0]].Cells[pos2XY[1]].Value + " ";
+                        StrSentence = StrSentence.Substring(2);
+                        continue;
+                    }
+
+                    Answer = Answer + dataGridView1.Rows[pos1XY[0]].Cells[pos2XY[1]].Value;
+                    Answer = Answer + dataGridView1.Rows[pos2XY[0]].Cells[pos1XY[1]].Value + " ";
+                    StrSentence = StrSentence.Substring(2);
+                }
+
+            for (int i = 0; i < Answer.Length; i++)
+            {
+
+                if (Answer[i].Equals(' '))
+                {
+                    Answer = Answer.Substring(0, i) + Answer.Substring(i + 1, Answer.Length - 1 - i);
+                    i--;
+                }
+            }
+            labelAnswer.Text = Answer;
+
+        }
+
+        private void ChBoxDecrypt_CheckedChanged(object sender, EventArgs e)
+        {
+            if (ChBoxDecrypt.Checked == true)
+                ChBoxEncrypt.Checked = false;
+
+        }
+
+        private void ChBoxEncrypt_CheckedChanged(object sender, EventArgs e)
+        {
+            if (ChBoxEncrypt.Checked == true)
+                ChBoxDecrypt.Checked = false;
+        }
+
+        private int[] LetterSearch(Char a)
+        {
+            a = Char.ToUpper(a);
+            String bas = "" + a;
+ 
+            int[] ba = new int[2];
+            for (int i = 0; i < 5; i++)          
+                for (int j = 0; j < 5; j++)
+                {
+                    if (dataGridView1.Rows[i].Cells[j].Value.Equals(bas))
+                    {
+                        ba[0] = i;
+                        ba[1] = j;
+                        return ba;
+                    }
+                }
+            return ba;
+        }
+
+    }
+}
