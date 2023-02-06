931090e0edd5387185e8075c371868988196db1e
MuradyanEduard <115282538+MuradyanEduard@users.noreply.github.com>
2023-02-07 00:53:53 +0400

    BlowFish First Commit

---
+++ C#/Cryptography/BlowFish/frmBlowFish/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace frmBlowFish
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.radioButtonE = new System.Windows.Forms.RadioButton();
+            this.radioButtonD = new System.Windows.Forms.RadioButton();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.textBoxKey = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.textBoxInput.Location = new System.Drawing.Point(12, 71);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(301, 45);
+            this.textBoxInput.TabIndex = 0;
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.textBoxOutput.Location = new System.Drawing.Point(12, 139);
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.Size = new System.Drawing.Size(301, 45);
+            this.textBoxOutput.TabIndex = 1;
+            // 
+            // radioButtonE
+            // 
+            this.radioButtonE.AutoSize = true;
+            this.radioButtonE.Location = new System.Drawing.Point(335, 122);
+            this.radioButtonE.Name = "radioButtonE";
+            this.radioButtonE.Size = new System.Drawing.Size(61, 17);
+            this.radioButtonE.TabIndex = 2;
+            this.radioButtonE.TabStop = true;
+            this.radioButtonE.Text = "Encrypt";
+            this.radioButtonE.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonD
+            // 
+            this.radioButtonD.AutoSize = true;
+            this.radioButtonD.Location = new System.Drawing.Point(402, 122);
+            this.radioButtonD.Name = "radioButtonD";
+            this.radioButtonD.Size = new System.Drawing.Size(62, 17);
+            this.radioButtonD.TabIndex = 3;
+            this.radioButtonD.TabStop = true;
+            this.radioButtonD.Text = "Decrypt";
+            this.radioButtonD.UseVisualStyleBackColor = true;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.btnStart.Location = new System.Drawing.Point(319, 143);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(166, 41);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // textBoxKey
+            // 
+            this.textBoxKey.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.textBoxKey.Location = new System.Drawing.Point(319, 71);
+            this.textBoxKey.Name = "textBoxKey";
+            this.textBoxKey.Size = new System.Drawing.Size(166, 45);
+            this.textBoxKey.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.label1.Location = new System.Drawing.Point(12, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 39);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Text";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.label2.Location = new System.Drawing.Point(312, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 39);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Key";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(535, 240);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxKey);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.radioButtonD);
+            this.Controls.Add(this.radioButtonE);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.textBoxInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.RadioButton radioButtonE;
+        private System.Windows.Forms.RadioButton radioButtonD;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox textBoxKey;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
