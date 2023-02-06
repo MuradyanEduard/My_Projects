cc7ac79a25bee8300af281a6a5d62cadcb77fce0
MuradyanEduard <115282538+MuradyanEduard@users.noreply.github.com>
2023-02-07 00:59:56 +0400

    Difi Helman First Commit

---
+++ C#/Cryptography/DifiHelman/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace DifiHelman
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gTxt = new System.Windows.Forms.TextBox();
+            this.nTxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.x1Txt = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.x2Txt = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.y1Label = new System.Windows.Forms.Label();
+            this.y2Label = new System.Windows.Forms.Label();
+            this.k1Label = new System.Windows.Forms.Label();
+            this.k2Label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.btnStart.Location = new System.Drawing.Point(422, 22);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(90, 33);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.Location = new System.Drawing.Point(21, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "G";
+            // 
+            // gTxt
+            // 
+            this.gTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.gTxt.Location = new System.Drawing.Point(54, 22);
+            this.gTxt.Name = "gTxt";
+            this.gTxt.Size = new System.Drawing.Size(100, 30);
+            this.gTxt.TabIndex = 2;
+            // 
+            // nTxt
+            // 
+            this.nTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.nTxt.Location = new System.Drawing.Point(255, 22);
+            this.nTxt.Name = "nTxt";
+            this.nTxt.Size = new System.Drawing.Size(100, 30);
+            this.nTxt.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label2.Location = new System.Drawing.Point(222, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "N";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label3.Location = new System.Drawing.Point(49, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "First User";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label4.Location = new System.Drawing.Point(229, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(126, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Second User";
+            // 
+            // x1Txt
+            // 
+            this.x1Txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.x1Txt.Location = new System.Drawing.Point(54, 126);
+            this.x1Txt.Name = "x1Txt";
+            this.x1Txt.Size = new System.Drawing.Size(100, 30);
+            this.x1Txt.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label5.Location = new System.Drawing.Point(21, 126);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 25);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "x1";
+            // 
+            // x2Txt
+            // 
+            this.x2Txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.x2Txt.Location = new System.Drawing.Point(272, 126);
+            this.x2Txt.Name = "x2Txt";
+            this.x2Txt.Size = new System.Drawing.Size(100, 30);
+            this.x2Txt.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label6.Location = new System.Drawing.Point(239, 126);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(33, 25);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "x2";
+            // 
+            // y1Label
+            // 
+            this.y1Label.AutoSize = true;
+            this.y1Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.y1Label.Location = new System.Drawing.Point(21, 173);
+            this.y1Label.Name = "y1Label";
+            this.y1Label.Size = new System.Drawing.Size(145, 25);
+            this.y1Label.TabIndex = 11;
+            this.y1Label.Text = "y1=G^x1modN ";
+            // 
+            // y2Label
+            // 
+            this.y2Label.AutoSize = true;
+            this.y2Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.y2Label.Location = new System.Drawing.Point(238, 173);
+            this.y2Label.Name = "y2Label";
+            this.y2Label.Size = new System.Drawing.Size(145, 25);
+            this.y2Label.TabIndex = 12;
+            this.y2Label.Text = "y2=G^x2modN ";
+            // 
+            // k1Label
+            // 
+            this.k1Label.AutoSize = true;
+            this.k1Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.k1Label.Location = new System.Drawing.Point(21, 217);
+            this.k1Label.Name = "k1Label";
+            this.k1Label.Size = new System.Drawing.Size(151, 25);
+            this.k1Label.TabIndex = 13;
+            this.k1Label.Text = "k1=y2^x1modN ";
+            // 
+            // k2Label
+            // 
+            this.k2Label.AutoSize = true;
+            this.k2Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.k2Label.Location = new System.Drawing.Point(238, 217);
+            this.k2Label.Name = "k2Label";
+            this.k2Label.Size = new System.Drawing.Size(151, 25);
+            this.k2Label.TabIndex = 14;
+            this.k2Label.Text = "k2=y1^x2modN ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 420);
+            this.Controls.Add(this.k2Label);
+            this.Controls.Add(this.k1Label);
+            this.Controls.Add(this.y2Label);
+            this.Controls.Add(this.y1Label);
+            this.Controls.Add(this.x2Txt);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.x1Txt);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.nTxt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.gTxt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox gTxt;
+        private System.Windows.Forms.TextBox nTxt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox x1Txt;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox x2Txt;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label y1Label;
+        private System.Windows.Forms.Label y2Label;
+        private System.Windows.Forms.Label k1Label;
+        private System.Windows.Forms.Label k2Label;
+    }
+}
+
---
+++ C#/Cryptography/DifiHelman/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DifiHelman
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int G,N,x1,x2,y1,y2,k1,k2;
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+           
+            Int32.TryParse(gTxt.Text,out G);
+            Int32.TryParse(nTxt.Text,out N);
+
+            Int32.TryParse(x1Txt.Text, out x1);
+            Int32.TryParse(x2Txt.Text, out x2);
+
+            y1 = (int)Math.Pow(G, x1) % N;
+            y2 = (int)Math.Pow(G, x2) % N;
+            y1Label.Text = "y1 = G^x1modN = " + y1.ToString();
+            y2Label.Text = "y2 = G^x2modN = " + y2.ToString();
+
+            k1 = (int)Math.Pow(y2, x1) % N;
+            k2 = (int)Math.Pow(y1, x2) % N;
+            k1Label.Text = "k1 = y2^x1modN = " + k1.ToString();
+            k2Label.Text = "k2 = y1^x2modN = " + k2.ToString();
+        }
+
+    }
+}
